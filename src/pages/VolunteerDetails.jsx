791acf4fca0f580108671cb7c53f39d88b3c1dd9
Kesharvani <shubhamkesharvani888@gmail.com>
2023-10-31 03:01:57 +0530

    feat:individual volunteer and delete handler

--- src/pages/VolunteerDetails.jsx
+++ src/pages/VolunteerDetails.jsx
@@ -1,5 +1,46 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
+import VolunteerModal from "../common/VolunteerModal";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteVolunteer } from "../features/volunteerSlice";
+import { useNavigate } from "react-router-dom";
 export default function VolunteerDetails() {
-  return <div>VolunteerDetails</div>;
+  const volunteerData = useSelector((state) => state.volunteer.volunteers);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const currentVolunteer = volunteerData.find(
+    (volunteer) => volunteer._id === id
+  );
+  if (!currentVolunteer) {
+    return <div>volunteer not found</div>;
+  }
+  const deleteHandler = (id) => {
+    dispatch(deleteVolunteer(id));
+    navigate("/");
+  };
+  return (
+    <div>
+      <h2>volunteer Details</h2>
+      <p>Name:{currentVolunteer?.name}</p>
+      <p>Contact:{currentVolunteer?.contact}</p>
+      <p>
+        Skills:
+        {currentVolunteer.skills.map((volunteer, index) => {
+          return <li key={index}>{volunteer}</li>;
+        })}
+      </p>
+      <p>
+        Area of Interest:
+        {currentVolunteer.areaOfInterest.map((interest, index) => {
+          return <li key={index}>{interest}</li>;
+        })}
+      </p>
+      <p>availibility:{currentVolunteer.availibility}</p>
+      <VolunteerModal volunteer={currentVolunteer} />
+      <button onClick={() => deleteHandler(currentVolunteer?._id)}>
+        Delete
+      </button>
+    </div>
+  );
 }
